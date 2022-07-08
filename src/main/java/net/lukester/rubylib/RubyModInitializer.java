7fe507f8261d002e0129ec5c5ebf6ede2848613c
lukester <gladiak602@gmail.com>
2022-07-07 21:29:58 -0600

    update

--- src/main/java/net/lukester/rubylib/RubyModInitializer.java
+++ src/main/java/net/lukester/rubylib/RubyModInitializer.java
@@ -6,14 +6,12 @@ import org.slf4j.LoggerFactory;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 
-public class RubyModInitializer implements ModInitializer {
+public abstract class RubyModInitializer implements ModInitializer {
     public static final String MOD_ID = "modid";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
-        
-	}
+	public void onInitialize() {}
 
     public String getModid() {
         return MOD_ID;
--- src/main/java/net/lukester/rubylib/mixin/ExampleMixin.java
+++ src/main/java/net/lukester/rubylib/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
 package net.lukester.rubylib.mixin;
 
-import net.lukester.rubylib.ExampleMod;
+import net.lukester.rubylib.RubyLib;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		RubyLib.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
