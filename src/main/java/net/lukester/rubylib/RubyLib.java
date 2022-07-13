1df7750bb556a7468f4cf0b8d793e606aaf1a58b
lukester <gladiak602@gmail.com>
2022-07-13 12:54:19 -0600

    fix

--- src/main/java/net/lukester/rubylib/RubyLib.java
+++ src/main/java/net/lukester/rubylib/RubyLib.java
@@ -3,9 +3,10 @@ package net.lukester.rubylib;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.fabricmc.api.ModInitializer;
 import net.lukester.rubylib.registry.BaseRegistry;
 
-public class RubyLib extends RubyModInitializer {
+public class RubyLib implements ModInitializer {
 
 	public static final String MOD_ID = "rubylib";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
