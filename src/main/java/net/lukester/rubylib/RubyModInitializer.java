fe9b130b8f802efea05cd6a5e91238169a54a5dc
lukester <gladiak602@gmail.com>
2022-07-13 12:43:52 -0600

    fix

--- src/main/java/net/lukester/rubylib/RubyModInitializer.java
+++ src/main/java/net/lukester/rubylib/RubyModInitializer.java
@@ -1,20 +1,20 @@
 package net.lukester.rubylib;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 
 public abstract class RubyModInitializer implements ModInitializer {
-    public static final String MOD_ID = "modid";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public final String MOD_ID;
+
+    public RubyModInitializer(String modid) {
+        this.MOD_ID = modid;
+    }
 
 	@Override
 	public void onInitialize() {}
 
     public String getModid() {
-        return MOD_ID;
+        return this.MOD_ID;
     }
 
     public Identifier getLocation(String id) {
